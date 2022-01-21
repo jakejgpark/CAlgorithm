4ba24c11e17fde9c0a437d82a5d31272adab8ee0
jakejgpark <ndamin221@gmail.com>
2022-01-21 23:44:54 +0900

    test

--- Project1/Project1/test2.c
+++ Project1/Project1/test2.c
@@ -1,15 +1,38 @@
-#include <stdio.h>
-#pragma warning(disable:4996)
-
-void output(int count)
-{
-		printf("hi");
-		return;         //값이 정의되지 않음.
-}
-
-
-int main(void)
-{
-	output(3);
-	return 0;
-}
+//#include <stdio.h>
+//#include <limits.h>
+//#pragma warning(disable:4996)
+//#define NUMBER 5
+//
+//
+//
+//int main(void)
+//{
+//	int i, max, min, index;
+//	int array[NUMBER];
+//	max = -10;
+//	min = 10;
+//
+//	// array[0] ~ array[4] : 총 5개가 들어갈 수 있는 배열
+//	for (i = 0; i < NUMBER; i++) {
+//		scanf("%d", &array[i]);
+//		if (max < array[i]) {
+//			max = array[i];
+//			index = i;
+//
+//		}
+//	}
+//	printf("%d %d", max, index + 1);
+//
+//	// array[0] ~ array[4] : 총 5개가 들어갈 수 있는 배열
+//	for (i = 0; i < NUMBER; i++) {
+//		scanf("%d", &array[i]);
+//		if (min > array[i]) {
+//			min = array[i];
+//			index = i;
+//		}
+//	}
+//
+//	printf("%d %d", min,index + 1);
+//
+//	return 0;
+//}
