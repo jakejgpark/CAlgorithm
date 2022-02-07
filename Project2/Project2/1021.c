ef9783269ade3bb4250b683da35cd2925714c8bb
jakejgpark <ndamin221@gmail.com>
2022-02-07 20:17:13 +0900

    TEST

---
+++ Project2/Project2/1021.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main()
+{
+	char x[100];
+	scanf("%s", x);
+	printf("%s", x);
+}
---
+++ Project2/Project2/1022.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d", n);
+}
