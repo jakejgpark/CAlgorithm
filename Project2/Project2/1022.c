0c4a2acc11a6741b965dd7d81465e9e96ffc9f86
JongGeun Park <ndamin221@gmail.com>
2022-02-08 23:20:57 +0900

    test

--- Project2/Project2/1022.c
+++ Project2/Project2/1022.c
@@ -2,7 +2,7 @@
 #pragma warning(disable:4996)
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	printf("%d", n);
+	char x[2001];
+	fgets(x,sizeof(x),stdin);
+	printf("%s", x);
 }
--- Project2/Project2/1026.c
+++ Project2/Project2/1026.c
@@ -2,9 +2,6 @@
 #pragma warning(disable:4996)
 int main()
 {
-	int x,y;
-	scanf("%d.%d", &x,&y);
-	printf("%d\n", x);
-	printf("%d", y);
+	printf("Hello\nWorld");
 	return 0;
 }
