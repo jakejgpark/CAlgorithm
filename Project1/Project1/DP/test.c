f34c0a9fa054fba0232fe5c66eef97f89c10788b
JGPark <ndamin221@gmail.com>
2022-06-04 20:38:13 +0900

    no message

--- Project1/Project1/DP/test.c
+++ Project1/Project1/DP/test.c
@@ -1,17 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS    // strcpy 보안 경고로 인한 컴파일 에러 방지
 #include <stdio.h>
+#include <string.h>    // strcpy 함수가 선언된 헤더 파일
+#include <stdlib.h>    // malloc, free 함수가 선언된 헤더 파일
+#define p1 (0xf002000)
 
+struct Person {    // 구조체 정의
+    char name[20];        // 구조체 멤버 1
+    int age;              // 구조체 멤버 2
+    char address[100];    // 구조체 멤버 3
+};
 
-int a=1;
-int b;
-int* c=&a;
-int port;
-int main(void)
+int main()
 {
-	for (int i=0; i<40; i++)
-	{
-		scanf("Port: %d", &port);
-		printf("P00_IN 주소값: %p\n", c);	
-		printf("port 0x100/4 계산: %x\n",port*0x100/4);
-		printf("Reg32 최종: %p\n", c+(port*0x100/4));
-	}
+    (struct Person *) p1;    // 구조체 포인터 선언, 메모리 할당
+	
+    return 0;
 }
